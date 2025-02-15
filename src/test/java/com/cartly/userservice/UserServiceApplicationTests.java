bd2803ff101094e2711bd0ed5a5fcfc81b4e24e4
Anshu Kailash <anshukailash@Mac.bbrouter>
2025-02-15 09:15:50 +0530

    first commit

---
+++ src/test/java/com/cartly/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cartly.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
